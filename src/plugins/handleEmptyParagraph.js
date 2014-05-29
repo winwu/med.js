79ca879045b54583d2d94a2482fbed67ff908d50
Po-Ying Chen <poying.me@gmail.com>
2014-05-29 17:54:30 +0800

    handleEmptyParagraph

--- src/plugins/handleEmptyParagraph.js
+++ src/plugins/handleEmptyParagraph.js
@@ -2,7 +2,7 @@ var handleEmptyParagraph = function (editor) {
   editor.on('walk', function (ctx) {
     var el = ctx.element;
 
-    if (utils.isEmpty(el)) {
+    if (!el.innerHTML.trim()) {
       if (utils.isType('paragraph', el)) {
         el.innerHTML = '<br type="_med_placeholder" />';
       } else if (utils.isType('section', el)) {
